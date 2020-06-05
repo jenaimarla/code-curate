2aa09cdfe7cc069b7e815fad7453aabcf8fd4c37
Shenaj Marla mshenaj@gmail.com <mshenaj@gmail.com>
2020-06-05 15:30:35 +0200

    font change

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module CodeCurate
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.assets.paths << Rails.root.join('lib', 'assets', 'fonts')
+config.assets.precompile += %w(.svg .eot .woff .ttf)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
